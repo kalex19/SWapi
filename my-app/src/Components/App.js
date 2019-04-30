132766d2c79167799bd844039ba78d639b359a00
Katherine Lewis <kale8432@colorado.edu>
2019-04-30 17:40:18 -0600

    Fetched random film

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,44 +1,52 @@
-import React, {Component} from 'react';
-import React from 'react';
-import Styles from './App.scss';
-import Header from '.Header.js';
-import Controls from '.Control.js';
-import CardContainer from '.CardContainer.js';
+import React, { Component } from 'react';
+import './App.scss';
+// import Header from './Header.js';
+// import Controls from './Control.js';
+// import CardContainer from './CardContainer.js';
 
 export default class App extends Component {
-  constructor()
-    super() 
-      this.state = {
-        film: {},
-        people: [],
-        planets: [],
-        vehicles: [],
-        Favorites: [],
-        Error: true,
-        Category: null
-      }
-//async
- ComponentDidMount() {
+	state = {
+		film: {},
+		people: [],
+		planets: [],
+		vehicles: [],
+		Favorites: [],
+		Error: true,
+		Category: null
+	};
+	//potentially move all state to comp and change app to func comp
 
+	componentDidMount() {
+		const range = 7;
+		const num = Math.floor(Math.random() * Math.floor(range)) + 1;
+		const filmUrl = `https://swapi.co/api/films/${num}`;
+		fetch(filmUrl)
+			.then(response => response.json())
+			.then(film =>
+				this.setState({ film }, () => {
+					console.log(this.state.film);
+				})
+			)
+			.catch(error => console.log(error));
+	}
+
+	//local storage
+
+	render() {
+		return (
+			<div className="App">
+				{/* <Header />
+				<Controls people={this.people} planets={this.planets} vehicles={this.vehicles} />
+				<CardContainer
+					people={this.people}
+					planets={this.planets}
+					vehicles={this.vehicles}
+					favorites={this.favorites}
+					error={this.error}
+				/> */}
+			</div>
+		);
+	}
+
+	//proptypes
 }
-
-//fetch calls
-
-//local storage
-
-render() {
-  return (
-    <div className="App">
-      <Header/>
-      <Controls  people={this.people} planets={this.planets} vehicles={this.vehicles}/>
-      <CardContainer people={this.people} planets={this.planets} vehicles={this.vehicles} favorites={this.favorites} error={this.error}/>
-    </div>
-  );
-
-}
-
-//proptypes
-}
-
-
-
--- my-app/src/Components/ScrollText.js
+++ my-app/src/Components/ScrollText.js
@@ -1,24 +1,33 @@
-import React, {Component} from 'react';
+import React, {
+  Component
+} from 'react';
 import Styles from './ScrollText.scss';
 
 
 export default ScrollText from Component {
 
 
-//const...
- //get movie info functionality
 
- //const Hello = ({name}) => <div>Hello {name}</div>
-render () {
-return(
-  <div>
-    {scrollText} //may change depending on the film selected
-    {movieTitle}
-    {movieReleasedate}
-  </div>
-)};
 
-ScrollText.propTypes = {
+  //const...
+  //get movie info functionality
+
+  //const Hello = ({name}) => <div>Hello {name}</div>
+  render() {
+    return ( <
+      div > {
+        scrollText
+      } //may change depending on the film selected
+      {
+        movieTitle
+      } {
+        movieReleasedate
+      } <
+      /div>
+    )
+  };
+
+  ScrollText.propTypes = {
     scrollText: PropTypes.string,
     moveiTitle: PropTypes.string,
     movieReleaseDate: PropTypes.string,
@@ -26,4 +35,3 @@ ScrollText.propTypes = {
   }
 
 }
-
