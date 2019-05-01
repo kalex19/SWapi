622c6fba2b4da64cb2e1518ba04234b81eec3bc1
Katherine Lewis <kale8432@colorado.edu>
2019-04-30 23:05:59 -0600

    Add font awesome icons

--- my-app/src/Components/Card.jsx
+++ my-app/src/Components/Card.jsx
@@ -9,6 +9,8 @@ const Card = props => {
 	if (props.category === 'people') {
 		people = (
 			<div className="cardHeader">
+				<i role="button" className="fab fa-rebel" />
+				{/* <img role="button" src={`${icon}`} alt="" /> */}
 				<h2>{props.info.name}</h2>
 				<p>{props.info.homeworld}</p>
 				<p>{props.info.species}</p>
@@ -19,6 +21,7 @@ const Card = props => {
 	if (props.category === 'planets') {
 		planets = (
 			<div className="cardHeader">
+				<i role="button" className="fab fa-rebel" />
 				<h2>{props.info.name}</h2>
 				<p>{props.info.terrain}</p>
 				<p>{props.info.population}</p>
@@ -31,6 +34,7 @@ const Card = props => {
 	if (props.category === 'vehicles') {
 		vehicles = (
 			<div className="cardHeader">
+				<i role="button" className="fab fa-rebel" />
 				<h2>{props.info.name}</h2>
 				<p>{props.info.model}</p>
 				<p>{props.info.class}</p>
--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -4,9 +4,11 @@ import './Header.scss';
 const Header = props => {
 	//favorite functionality
 
+	let num = 6;
 	return (
 		<div>
 			<h1> Star Wars </h1>
+			<input type="button" className="viewFavorites" value="View Favorites" />
 		</div>
 	);
 };
